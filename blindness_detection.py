af77162369b628674c6decf536a19da4e61b5ab3
ShivanshSharma12 <142719831+ShivanshSharma12@users.noreply.github.com>
2023-11-22 20:46:34 +0530

    Add files via upload

---
+++ blindness_detection.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import time
+import shutil
+import pathlib
+import itertools
+
+# import data handling tools
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_style('darkgrid')
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# import Deep learning Libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout, BatchNormalization
+from tensorflow.keras import regularizers
+
+
+# In[2]:
+
+
+# Ignore Warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+print ('modules loaded')
+
+
+# ## Read data and store it in dataframe
+
+# In[5]:
+
+
+# Generate data paths with labels
+data_dir = r"C:\Users\SHIVANSH SHARMA\Desktop\codeclause\blindness detection/gaussian_filtered_images/gaussian_filtered_images"
+filepaths = []
+labels = []
+
+folds = os.listdir(data_dir)
+for fold in folds:
+    foldpath = os.path.join(data_dir, fold)  
+    # skip export.pkl file
+    if pathlib.Path(foldpath).suffix == '':
+        filelist = os.listdir(foldpath)
+
+        for file in filelist:
+            fpath = os.path.join(foldpath, file)
+            filepaths.append(fpath)
+            labels.append(fold)
+
+# Concatenate data paths with labels into one dataframe
+Fseries = pd.Series(filepaths, name= 'filepaths')
+Lseries = pd.Series(labels, name='labels')
+df = pd.concat([Fseries, Lseries], axis= 1)
+
+
+# ## Split dataframe into train, valid, and test
+
+# In[6]:
+
+
+# train dataframe
+train_df, dummy_df = train_test_split(df,  train_size= 0.8, shuffle= True, random_state= 123)
+
+# valid and test dataframe
+valid_df, test_df = train_test_split(dummy_df,  train_size= 0.6, shuffle= True, random_state= 123)
+
+
+# ## Create image data generator
+
+# In[7]:
+
+
+# crobed image size
+batch_size = 16
+img_size = (224, 224)
+channels = 3
+img_shape = (img_size[0], img_size[1], channels)
+
+tr_gen = ImageDataGenerator()
+ts_gen = ImageDataGenerator()
+
+train_gen = tr_gen.flow_from_dataframe( train_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                    color_mode= 'rgb', shuffle= True, batch_size= batch_size)
+
+valid_gen = ts_gen.flow_from_dataframe( valid_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                    color_mode= 'rgb', shuffle= True, batch_size= batch_size)
+test_gen = ts_gen.flow_from_dataframe( test_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                    color_mode= 'rgb', shuffle= False, batch_size= batch_size)
+
+
+# ## Show sample from train data
+
+# In[8]:
+
+
+g_dict = train_gen.class_indices      # defines dictionary {'class': index}
+classes = list(g_dict.keys())       # defines list of dictionary's kays (classes), classes names : string
+images, labels = next(train_gen)      # get a batch size samples from the generator
+
+plt.figure(figsize= (20, 20))
+
+for i in range(16):
+    plt.subplot(4, 4, i + 1)
+    image = images[i] / 255       # scales data to range (0 - 255)
+    plt.imshow(image)
+    index = np.argmax(labels[i])  # get image index
+    class_name = classes[index]   # get class of image
+    plt.title(class_name, color= 'blue', fontsize= 12)
+    plt.axis('off')
+plt.show()
+
+
+# ## Generic Model Creation
+
+# In[9]:
+
+
+img_size = (224, 224)
+channels = 3
+img_shape = (img_size[0], img_size[1], channels)
+class_count = len(list(train_gen.class_indices.keys())) # to define number of classes in dense layer
+
+# create pre-trained model (you can built on pretrained model such as :  efficientnet, VGG , Resnet )
+# we will use efficientnetb3 from EfficientNet family.
+base_model = tf.keras.applications.efficientnet.EfficientNetB0(include_top= False, weights= "imagenet", input_shape= img_shape, pooling= 'max')
+
+model = Sequential([
+    base_model,
+    BatchNormalization(axis= -1, momentum= 0.99, epsilon= 0.001),
+    Dense(256, kernel_regularizer= regularizers.l2(l= 0.016), activity_regularizer= regularizers.l1(0.006),
+                bias_regularizer= regularizers.l1(0.006), activation= 'relu'),
+    Dropout(rate= 0.45, seed= 123),
+     Dense(class_count, activation= 'softmax')
+])
+
+
+model.compile(Adamax(learning_rate= 0.001), loss= 'binary_crossentropy', metrics= ['accuracy'])
+
+model.summary()
+
+
+# ## train model
+
+# In[11]:
+
+
+epochs =20    #number of all epochs in training
+
+history = model.fit(x= train_gen, epochs= epochs, verbose= 1, validation_data= valid_gen, 
+                    validation_steps= None, shuffle= False)
+
+
+# ###### 
+
+# In[12]:
+
+
+# Define needed variables
+tr_acc = history.history['accuracy']
+tr_loss = history.history['loss']
+val_acc = history.history['val_accuracy']
+val_loss = history.history['val_loss']
+index_loss = np.argmin(val_loss)
+val_lowest = val_loss[index_loss]
+index_acc = np.argmax(val_acc)
+acc_highest = val_acc[index_acc]
+Epochs = [i+1 for i in range(len(tr_acc))]
+loss_label = f'best epoch= {str(index_loss + 1)}'
+acc_label = f'best epoch= {str(index_acc + 1)}'
+
+# Plot training history
+plt.figure(figsize= (20, 8))
+plt.style.use('fivethirtyeight')
+plt.subplot(1, 2, 1)
+plt.plot(Epochs, tr_loss, 'r', label= 'Training loss')
+plt.plot(Epochs, val_loss, 'g', label= 'Validation loss')
+plt.scatter(index_loss + 1, val_lowest, s= 150, c= 'blue', label= loss_label)
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(Epochs, tr_acc, 'r', label= 'Training Accuracy')
+plt.plot(Epochs, val_acc, 'g', label= 'Validation Accuracy')
+plt.scatter(index_acc + 1 , acc_highest, s= 150, c= 'blue', label= acc_label)
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout
+plt.show()
+
+
+# ## evaluate model
+
+# In[13]:
+
+
+ts_length = len(test_df)
+test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length%n == 0 and ts_length/n <= 80]))
+test_steps = ts_length // test_batch_size
+
+train_score = model.evaluate(train_gen, steps= test_steps, verbose= 1)
+valid_score = model.evaluate(valid_gen, steps= test_steps, verbose= 1)
+test_score = model.evaluate(test_gen, steps= test_steps, verbose= 1)
+
+print("Train Loss: ", train_score[0])
+print("Train Accuracy: ", train_score[1])
+print('-' * 20)
+print("Validation Loss: ", valid_score[0])
+print("Validation Accuracy: ", valid_score[1])
+print('-' * 20)
+print("Test Loss: ", test_score[0])
+print("Test Accuracy: ", test_score[1])
+
+
+# In[14]:
+
+
+preds = model.predict_generator(test_gen)
+y_pred = np.argmax(preds, axis=1)
+
+
+# ## Classification Report
+
+# In[15]:
+
+
+print(classification_report(test_gen.classes, y_pred, target_names= classes))
+
+
+# In[ ]:
+
+
+
+
